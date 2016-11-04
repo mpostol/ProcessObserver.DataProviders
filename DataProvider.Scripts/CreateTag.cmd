046ff45dc4fa1d24d201762e3da1d8fd8cc09609
Mariusz <mpostol@cas.eu>
2016-11-04 20:55:29 +0000

    OPC VIewer Installing license has failed - working on the DataProvider  - added project generating script creating tag. Issue #: 4884

---
+++ DataProvider.Scripts/CreateTag.cmd
@@ -0,0 +1,13 @@
+
+rem//  $LastChangedDate:  $
+rem//  $Rev:  $
+rem//  $LastChangedBy:  $
+rem//  $URL:  $
+rem//  $Id:  $
+
+
+svn mkdir svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.DataProvider.rel_3_80_03  -m "created new svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.DataProvider.rel_3_80_03"
+svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/CommServer.DataProvider/ svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.DataProvider.rel_3_80_03 -m "created copy svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.DataProvider.rel_3_80_03"
+
+pause ....
+
---
+++ DataProvider.Scripts/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿//_______________________________________________________________
+//  Title   :  Assembly info for: CAS.CommServer.DataProvider.Scripts
+//  System  : Microsoft VisualStudio 2015 / C#
+//  $LastChangedDate:  $
+//  $Rev:  $
+//  $LastChangedBy: $
+//  $URL:  $
+//  $Id:  $
+//
+//  Copyright (c) 2000-2016 CAS LODZ POLAND
+//  +48 (42) 686 25 47
+//  techsupp@cas.eu
+//  www.cas.eu
+//_______________________________________________________________
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CAS.CommServer.DataProvider.Scripts")]
+[assembly: AssemblyDescription("Scripts Prototyping Workspace for CAS.CommServer.DataProvider.Scripts")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("CAS CommServer Family")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2016 CAS LODZ POLAND")]
+[assembly: AssemblyTrademark("CommServer")]
+[assembly: AssemblyCulture("")]
+[assembly: Guid("F03FAECE-6874-418B-93FA-FC8CA11CBF59")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("3.80.03.*")]
+[assembly: AssemblyFileVersion("3.80.03")]
+
