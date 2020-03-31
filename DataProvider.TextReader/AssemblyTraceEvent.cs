fe8c4f7e6770a64cbf6f6a8cba00e7e8743faead
mpostol <mpostol@cas.eu>
2020-03-31 14:01:28 +0200

    Move reactive programming example to TP #8
    - refactoring the code to harmonize with TP

--- DataProvider.TextReader/AssemblyTraceEvent.cs
+++ DataProvider.TextReader/AssemblyTraceEvent.cs
@@ -48,18 +48,4 @@ namespace CAS.CommServer.DataProvider.TextReader
     internal TraceSource TraceSource => m_TraceEventInternal.Value;
   }
 
-  /// <summary>
-  /// Interface ITraceSource - declares basic functionality for the component behavior tracing.
-  /// </summary>
-  public interface ITraceSource
-  {
-    /// <summary>
-    /// a trace event message to the trace listeners in the System.Diagnostics.TraceSource.Listeners collection using the specified event type and event identifier.
-    /// </summary>
-    /// <param name="eventType">One of the enumeration values that specifies the event type of the trace data.</param>
-    /// <param name="id">A numeric identifier for the event.</param>
-    /// <param name="message"> The trace message to write.</param>
-    /// <exception cref="T:System.ObjectDisposedException">An attempt was made to trace an event during finalization.</exception>
-    void TraceMessage(TraceEventType eventType, int id, string message);
-  }
 }
